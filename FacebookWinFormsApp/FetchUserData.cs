ff62dbd5daad5289cc6a1b48c9dce4e9973cd788
shookran <shachar.almagors@gmail.com>
2021-10-26 16:33:14 +0300

    Initial commit

---
+++ FacebookWinFormsApp/FetchUserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+    class FetchUserData
+    {
+        User m_LoggedInUser;
+        /*
+        public FacebookObjectCollection<Post> fetchPosts()
+        {
+        }
+
+        public FacebookObjectCollection<Album> fetchAlbums()
+        {
+
+        }
+        */
+    }
+}
